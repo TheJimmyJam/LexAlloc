ff857a4ce34f429eed79079cbb944a3d61110318
TheJimmyJam <wcannon83@gmail.com>
2026-04-23 23:32:36 -0500

    Update src/components/Layout.jsx

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -64,7 +64,7 @@ export default function Layout() {
       <aside className={`fixed inset-y-0 left-0 z-30 w-60 bg-white border-r border-slate-200 flex flex-col transform transition-transform duration-200 ease-in-out lg:translate-x-0 lg:static lg:flex ${sidebarOpen ? 'translate-x-0' : '-translate-x-full'}`}>
         {/* Logo */}
         <div className="flex items-center px-4 h-16 border-b border-slate-200 flex-shrink-0">
-          <img src="/logo.svg" alt="LexAlloc" className="h-9 w-auto" />
+          <img src="/logo.svg" alt="LexAlloc" className="h-12 w-auto" />
         </div>
 
         {/* Org badge */}
